b5c6f13ed5f87021a8dd183ce8ccd395181b8081
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-03-29 22:19:48 +0000

    Modified some files

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,6 +2,7 @@
 exec { 'command':
   command => 'apt-get -y update;
   apt-get -y install nginx;
-  sudo sed -i "/server_name _;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  sudo sed -i "//a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   service nginx restart',
+  provider => shell,
 }
