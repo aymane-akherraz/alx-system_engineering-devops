fe2ed927cd52463120894d319518d89b17729d52
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-03-29 22:18:41 +0000

    Modified some files

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,7 @@
 # Creates a custom HTTP header response
-
-exec { 'update system':
-  command => '/usr/bin/apt-get update',
-}
-
-package { 'nginx':
-  ensure => installed,
-  require => Exec['update system']
-}
-
-exec { 'set_custom_header':
-  command => '/usr/bin/sudo sed -i "/server_name _;/a add_header X-Served-By $(hostname);" /etc/nginx/sites-available/default'
-}
-
-service { 'nginx':
-  ensure  => running,
-  restart => true,
+exec { 'command':
+  command => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/server_name _;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
 }
