2b45462d97f17cc5af9e5335a7c166c8a8f8732f
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-05-07 20:21:45 +0100

    Modified some files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,6 +6,9 @@ import requests
 def number_of_subscribers(subreddit):
     """ Returns the number of subscribers for a given subreddit """
 
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     r = requests.get('https://www.reddit.com/r/{}/about.json'
                      .format(subreddit),
                      headers={"User-Agent": "My-Agent"},
