6db12d19d3d3228ef65819cd8e3ad81f28efe9ad
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-05-07 21:19:02 +0100

    Modified some files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,15 +6,11 @@ import requests
 def number_of_subscribers(subreddit):
     """ Returns the number of subscribers for a given subreddit """
 
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
     r = requests.get('https://www.reddit.com/r/{}/about.json'
                      .format(subreddit),
-                     headers={"User-Agent": "My-Agent"},
                      allow_redirects=False)
 
-    try:
-        return r.json()["data"]["subscribers"]
-    except Exception:
+    if r.status_code == 404:
         return 0
+
+    return r.json()["data"]["subscribers"]
