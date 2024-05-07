ca26d1b761583187e42f56df4c0cbfe78f8ba1d8
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-05-07 19:42:47 +0100

    Modified 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,10 @@ def number_of_subscribers(subreddit):
     """ Returns the number of subscribers for a given subreddit """
 
     r = requests.get('https://www.reddit.com/r/{}/about.json'
-                     .format(subreddit), allow_redirects=False)
+                     .format(subreddit),
+                     headers={"User-Agent": "My-Agent"},
+                     allow_redirects=False)
+
     if r.status_code != 200:
         return 0
 
