122fb49e787169e2b3c341b6c23de20c758a4c8d
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-05-08 11:02:13 +0100

    Modified some files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,12 @@ def number_of_subscribers(subreddit):
 
     r = requests.get('https://www.reddit.com/r/{}/about.json'
                      .format(subreddit),
-                     headers={"User-Agent": "My-User-Agent"},
+                     headers={"User-Agent": "0x16-api_advanced:project:\
+                     v1.0.0 (by /u/aymane)"},
                      allow_redirects=False)
-    if r.status_code != 200:
+
+    if r.status_code == 404:
         return 0
 
-    return r.json().get("data").get("subscribers")
+    res = r.json().get("data")
+    return res.get("subscribers")
