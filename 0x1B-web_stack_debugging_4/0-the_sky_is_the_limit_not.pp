72275678960cc9bda0f5217e08bc957189b0ef62
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-05-16 11:16:19 +0100

    Modified 0-

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # fix nginx error
 
 exec { 'fix-nginx-err':
-  command => 'sed -i "s/15/4096/g" /etc/default/nginx',
-  path    => ['/bin', '/usr/bin'],
+  command  => 'sed -i "s/15/4096/" /etc/default/nginx',
+  provider => 'shell'
 }
 
 exec { 'restart-nginx':
-  command => 'service nginx restart',
-  path    => ['/bin', '/usr/bin'],
+  command  => 'service nginx restart',
+  provider => 'shell'
 }
